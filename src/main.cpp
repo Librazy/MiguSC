16c2e7174f696104d17e4368f58b2abe8e86ca35
Librazy <librazy@foxmail.com>
2016-10-05 19:23:20 +0800

    -

--- src/main.cpp
+++ src/main.cpp
@@ -2,203 +2,19 @@
 #include <windows.h>
 #endif
 
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-
-#include <vector>
-#include <memory>
-
+#include "util.h"
 #include "triangle/util.h"
-#include "triangle/triangle_internal.h"
 
-#include <OpenMesh/Core/IO/MeshIO.hh>
-#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
-
-
-using OpenMeshT = OpenMesh::DefaultTraits;
-
-using TriMs = OpenMesh::TriMesh_ArrayKernelT<>;
-
-template<typename ... Args>
-static std::string string_format(const std::string& format, Args ... args)
-{
-	auto size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
-	std::unique_ptr<char[]> buf(new char[size]);
-	snprintf(buf.get(), size, format.c_str(), args ...);
-	return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
-}
-
-static int bound(int i, int a, int b)
-{
-	return std::min(std::max(i, std::min(a, b)), std::max(a, b));
-}
-
-static cv::Scalar getInverseColor(cv::Scalar c)
-{
-	return cv::Scalar(255, 255, 255, 0) - c;
-}
-
-struct imgLine
-{
-	size_t index;
-	double length;
-	size_t div;
-
-	imgLine(size_t index, double length, size_t div
-	) :index(index), length(length), div(div)
-	{}
-
-	bool operator<(imgLine const& a) const noexcept
-	{
-		return length / div > a.length / a.div;
-	}
-};
-
-struct imgText
-{
-	std::string text;
-	cv::Point orgi;
-	cv::Scalar color;
-
-	imgText(std::string text, cv::Point orgi, cv::Scalar color
-	) :text(text), orgi(orgi), color(color)
-	{}
-};
+#include <opencv2/imgcodecs.hpp>
+#include <vector>
 
 cv::Mat src;
 cv::Mat dst;
 cv::Mat ori;
-int n = 0;
 
-std::vector<cv::Point2f> points;
-std::vector<cv::Point2f> oriPoints;
+std::vector<cv::Point2d> points;
+std::vector<cv::Point2d> oriPoints;
 std::vector<imgText> labels;
-
-static const std::string mainWindowName = "Affine with CUDA support";
-
-static bool if_in_range(cv::Point2f point)
-{
-	return !points.empty() && norm(point - points[0]) < 30;
-}
-
-static void draw_point(cv::Mat& img, cv::Point fp, cv::Scalar color, int rad = 2)
-{
-	circle(img, fp, rad, color, CV_FILLED, CV_AA, 0);
-}
-
-static void draw_line(cv::Mat& img, cv::Point p1, cv::Point p2, cv::Scalar color = cv::Scalar(0, 0, 0))
-{
-	line(img, p1, p2,
-		color, 2, cv::LineTypes::LINE_AA);
-}
-
-static void draw_text(cv::Mat& img, cv::String text, cv::Point org, cv::Scalar color = cv::Scalar(0, 0, 0))
-{
-	int baseline;
-	auto size = getTextSize(text, cv::HersheyFonts::FONT_HERSHEY_COMPLEX, 0.5, 1, &baseline);
-	cv::Point tmp_pt = { bound(org.x, 0, src.cols - size.width),
-					bound(org.y, size.height + baseline, src.rows - 1 - baseline) };
-	putText(src, text, tmp_pt, cv::HersheyFonts::FONT_HERSHEY_COMPLEX, 0.5, color, 1, cv::LineTypes::LINE_AA);
-}
-
-static void draw_text(cv::Mat& img, imgText label)
-{
-	draw_text(img, label.text, label.orgi, label.color);
-}
-//
-////邻接顶点的余切权重计算
-//void CotangentWeights(TriMs*TMesh, int vIndex, std::vector<double>&vweight, double &WeightSum, bool bNormalize)//计算一阶邻近点的各自cottan权重
-//{
-//	int NeighborNumber = TMesh->neighbors[vIndex].size();
-//	vweight.resize(NeighborNumber);
-//	WeightSum = 0;
-//	std::vector<int>&NeiV = TMesh->neighbors[vIndex];
-//	for (int i = 0; i < NeighborNumber; i++)
-//	{
-//		int j_nei = NeiV[i];
-//		std::vector<int>tempnei;
-//		Co_neighbor(TMesh, vIndex, j_nei, tempnei);
-//		double cotsum = 0.0;
-//		for (int j = 0; j < tempnei.size(); j++)
-//		{
-//			vec vivo = TMesh->vertices[vIndex] - TMesh->vertices[tempnei[j]];
-//			vec vjvo = TMesh->vertices[j_nei] - TMesh->vertices[tempnei[j]];
-//			double dotvector = vivo DOT vjvo;
-//			dotvector = dotvector / sqrt(len2(vivo)*len2(vjvo) - dotvector*dotvector);
-//			cotsum += dotvector;
-//		}
-//		vweight[i] = cotsum / 2.0;
-//		WeightSum += vweight[i];
-//	}
-//
-//	if (bNormalize)
-//	{
-//		for (int k = 0; k < NeighborNumber; ++k)
-//		{
-//			vweight[k] /= WeightSum;
-//		}
-//		WeightSum = 1.0;
-//	}
-//}
-//
-////获取两顶点的共同邻接顶点
-//void Co_neighbor(TriMs *Tmesh, int u_id, int v_id, std::vector<int>&co_neiv)
-//{
-//	Tmesh->need_adjacentedges();
-//	std::vector<int>&u_id_ae = Tmesh->adjancetedge[u_id];
-//	int en = u_id_ae.size();
-//	Tedge Co_Edge;
-//	for (int i = 0; i < en; i++)
-//	{
-//		Tedge &ae = Tmesh->m_edges[u_id_ae[i]];
-//		int opsi = ae.opposite_vertex(u_id);
-//		if (opsi == v_id)
-//		{
-//			Co_Edge = ae;
-//			break;
-//		}
-//	}
-//	for (int i = 0; i < Co_Edge.m_adjacent_faces.size(); i++)
-//	{
-//		TriMs::Face af = Tmesh->faces[Co_Edge.m_adjacent_faces[i]];
-//		for (int j = 0; j < 3; j++)
-//		{
-//			if ((af[j] != u_id) && (af[j] != v_id))
-//			{
-//				co_neiv.push_back(af[j]);
-//			}
-//		}
-//	}
-//}
-////计算拉普拉斯矩阵
-//void Get_Laplace_Matrix()
-//{
-//	int vn = m_BaseMesh->vertices.size();
-//	int count0 = 0;
-//	std::vector<int>begin_N(vn);
-//	for (int i = 0; i < vn; i++)
-//	{
-//		begin_N[i] = count0;
-//		count0 += m_BaseMesh->neighbors[i].size() + 1;
-//	}
-//	typedef Eigen::Triplet<double> T;
-//	std::vector<T> tripletList(count0);
-//	for (int i = 0; i < vn; i++)
-//	{
-//		VProperty & vi = m_vertices[i];
-//		tripletList[begin_N[i]] = T(i, i, -vi.VSumWeight);
-//		int nNbrs = vi.VNeighbors.size();
-//		for (int k = 0; k < nNbrs; ++k)
-//		{
-//			tripletList[begin_N[i] + k + 1] = T(vi.VNeighbors[k], i, vi.VNeiWeight[k]);
-//		}
-//	}
-//	m_Laplace_Matrix.resize(vn, vn);
-//	m_Laplace_Matrix.setFromTriplets(tripletList.begin(), tripletList.end());
-//
-//}
 
 double cot(OpenMeshT::Point p1, OpenMeshT::Point p2, OpenMeshT::Point pc)
 {
@@ -258,8 +74,7 @@ double cot_weight(TriMs& mesh, OpenMesh::VertexHandle p1, OpenMesh::VertexHandle
 	return accumulate(ws.begin(), ws.end(), 0.0);
 }
 
-
-cv::Point2d laplace_cord(TriMs& mesh, OpenMesh::VertexHandle p1)
+cv::Point2d laplace_cord(TriMs& mesh, OpenMesh::VertexHandle p1, std::vector<Tpt_d>& out_triplet)
 {
 	auto ns = nerghbor(mesh, p1);
 	auto mp1 = mesh.point(p1);
@@ -268,6 +83,7 @@ cv::Point2d laplace_cord(TriMs& mesh, OpenMesh::VertexHandle p1)
 	auto nweigs = std::vector<double>();
 	auto ndetas = std::vector<cv::Point2d>();
 
+	out_triplet.emplace_back(Tpt_d(p1.idx(), p1.idx(), 1));
 
 	for(auto n : ns) {
 		auto p = mesh.point(n);
@@ -277,13 +93,24 @@ cv::Point2d laplace_cord(TriMs& mesh, OpenMesh::VertexHandle p1)
 
 	auto s = accumulate(nweigs.begin(), nweigs.end(), 0.0);
 	auto x = cv::Point2d(0.0, 0.0);
-	for (size_t i = 0; i != ncords.size();++i) {
+
+	size_t num = ncords.size();
+
+	for (size_t i = 0; i != num;++i) {
+		out_triplet.emplace_back(Tpt_d(p1.idx(), ns[i].idx(), -nweigs[i]/s));
+	}
+
+	for (size_t i = 0; i != num;++i) {
 		x += nweigs[i] * ncords[i] / s;
 	}
 
+	//for (size_t i = 0; i != num; ++i) {
+	//	out_triplet.emplace_back(Tpt_d(p1.idx(), ns[i].idx(), -1.0 / num));
+	//}
+	
 	// x = cv::Point2d(0.0, 0.0);
-	//for (size_t i = 0; i != ncords.size(); ++i) {
-	//	x += ncords[i] / static_cast<double>(ncords.size());
+	//for (size_t i = 0; i != num; ++i) {
+	//	x += ncords[i] / static_cast<double>(num);
 	//}
 
 	return cp1 - x;
@@ -295,7 +122,7 @@ static int triangle_create()
 	auto ctx = triangle_context_create();
 	auto in = new triangleio();
 	reset_triangleio(in);
-	triangle_context_options(ctx, "pq15a1024");
+	triangle_context_options(ctx, "pq15a3024");
 	in->numberofsegments = oriPoints.size();
 	in->numberofpoints = oriPoints.size();
 
@@ -407,30 +234,63 @@ static int triangle_create()
 			triangleloop.tri = triangletraverse(m);
 			elementnumber++;
 		}
-		if (trisegsFin.size() >= 1) {
-			for (auto se : trisegsFin) {
-				line(dst, vertexsFin[se.x] , vertexsFin[se.y], cv::Scalar(0, 255, 255), 1, CV_AA, 0);
+		
+		auto laplace_cords = std::vector<cv::Point2d>();
+
+		const auto num = meshVertexs.size();
+
+		auto delta_m = cv::Mat_<double>(cv::Size(2, num + 3), 0);
+
+		auto ans_m = cv::Mat_<double>(cv::Size(2, num + 3), 0);
+
+		auto ori_m = cv::Mat_<double>(cv::Size(2, num), 0);
+
+		auto t_m = cv::Mat_<double>(cv::Size(num, num + 3),0);
+
+		for(auto i = 0;i != num; ++i) {
+			ori_m[meshVertexs[i].idx()][0] = mesh.point(meshVertexs[i])[0];
+			ori_m[meshVertexs[i].idx()][1] = mesh.point(meshVertexs[i])[1];
+			t_m[meshVertexs[i].idx()][meshVertexs[i].idx()] = 1.0;
+			auto triplet = std::vector<Tpt_d>();
+			laplace_cords.emplace_back(laplace_cord(mesh, meshVertexs[i], triplet));
+			delta_m[meshVertexs[i].idx()][0] = laplace_cords.back().x;
+			delta_m[meshVertexs[i].idx()][1] = laplace_cords.back().y;
+			for(auto tr : triplet) {
+				t_m[meshVertexs[std::get<0>(tr)].idx()][meshVertexs[std::get<1>(tr)].idx()] = std::get<2>(tr);
 			}
 		}
-		if (segmentsFin.size() >= 1) {
-			for (auto se : segmentsFin) {
-				line(dst, vertexsFin[se.x] , vertexsFin[se.y] , cv::Scalar(0,0,255), 1, CV_AA, 0);
-			}
-		}
 
-		auto op = mesh.point(meshVertexs[14]);
-		draw_point(dst, cv::Point2d(op[0], op[1]), cv::Scalar(0, 255, 21, 0), 6);
-		for (auto vohit = mesh.voh_iter(meshVertexs[14]); vohit.is_valid(); ++vohit) {
-			auto p = mesh.point(mesh.to_vertex_handle(*vohit));
-			printf("%lf %lf %lf\n", p[0], p[1], p[2]);
-			draw_point(dst, cv::Point2d(p[0], p[1]), cv::Scalar(0, 255, 21, 0), 6);
-		}
+		t_m[num + 0][meshVertexs[0].idx()] = 1;
+		t_m[num + 1][meshVertexs[1].idx()] = 1;
+		t_m[num + 2][meshVertexs[2].idx()] = 1;
 
-		auto lap =  laplace_cord(mesh, meshVertexs[14]);
-		printf("%lf %lf\n", lap.x, lap.y);
-		draw_point(dst, cv::Point2d(op[0], op[1]) - lap, cv::Scalar(0, 25, 201, 0), 3);
+		delta_m[num + 0][0] = mesh.point(meshVertexs[0])[0];
+		delta_m[num + 0][1] = mesh.point(meshVertexs[0])[1];
+		delta_m[num + 1][0] = mesh.point(meshVertexs[1])[0];
+		delta_m[num + 1][1] = mesh.point(meshVertexs[1])[1];
+		delta_m[num + 2][0] = mesh.point(meshVertexs[2])[0];
+		delta_m[num + 2][1] = mesh.point(meshVertexs[2])[1];
 
-		printf("%lf %lf\n", (cv::Point2d(op[0], op[1]) - lap).x, (cv::Point2d(op[0], op[1]) - lap).y);
+		solve(t_m, delta_m, ans_m);
+
+		std::cout << "t_m" << std::endl;
+		std::cout << t_m << std::endl << std::endl;
+
+		std::cout << "ori_m" << std::endl;
+		std::cout << ori_m << std::endl << std::endl;
+
+		std::cout << "t_m * ori_m" << std::endl;
+		std::cout << t_m * ori_m << std::endl << std::endl;
+
+		std::cout << "delta_m" << std::endl;
+		std::cout << delta_m << std::endl << std::endl;
+
+		std::cout << "ans_m" << std::endl;
+		std::cout << ans_m << std::endl << std::endl;
+
+		std::cout << "t_m * ans_m" << std::endl;
+		std::cout << t_m * ans_m << std::endl << std::endl;
+
 		cv::namedWindow("x", 1);
 		imshow("x", dst);
 
@@ -452,7 +312,7 @@ static void mouse_event_handle(int event, int x, int y, int flags, void* ustc)
 {
 	cv::Point pt;
 	std::string temp;
-
+	static int n = 0;
 	auto clrPoint = cv::Scalar(255, 0, 0, 0);
 	auto clrText = cv::Scalar(255, 200, 0, 0);
 	if (event == CV_EVENT_MOUSEMOVE) {
@@ -462,7 +322,7 @@ static void mouse_event_handle(int event, int x, int y, int flags, void* ustc)
 		y = bound(y, 0, src.rows - 1);
 		pt = cv::Point(x, y);
 
-		if (points.size() >= 3 && if_in_range(pt)) {
+		if (points.size() >= 3 && if_in_range(pt, points)) {
 			pt = points[0];
 			temp = string_format("%s", "");
 		}
@@ -470,8 +330,8 @@ static void mouse_event_handle(int event, int x, int y, int flags, void* ustc)
 			temp = string_format("%d (%d,%d)", n + 1, pt.x, pt.y);
 		}
 
-		draw_text(src, temp, pt, getInverseColor(clrText));
-		draw_point(src, pt, getInverseColor(clrPoint));
+		draw_text(src, temp, pt, get_inverseColor(clrText));
+		draw_point(src, pt, get_inverseColor(clrPoint));
 
 		for (auto p : points) {
 			draw_point(src, p, clrPoint);
@@ -496,7 +356,7 @@ static void mouse_event_handle(int event, int x, int y, int flags, void* ustc)
 
 		pt = cv::Point(x, y);
 
-		if (points.size() >= 3 && if_in_range(pt)) {
+		if (points.size() >= 3 && if_in_range(pt, points)) {
 			imshow(mainWindowName, src);
 			cv::setMouseCallback(mainWindowName, nullptr, nullptr);
 			//cv::setMouseCallback(mainWindowName, drag_point, nullptr);
@@ -517,6 +377,7 @@ static void mouse_event_handle(int event, int x, int y, int flags, void* ustc)
 	}
 }
 
+#define _DEBUG
 #ifdef _MSC_VER
 #ifdef _DEBUG
 int main()
@@ -531,6 +392,7 @@ int WinMain(HINSTANCE hInstance,
 int main()
 #endif// _MSC_VER
 {
+
 	cv::namedWindow(mainWindowName, 1);
 
 	ori = cv::imread("qwe.jpg");
---
+++ src/util.h
@@ -0,0 +1,99 @@
+#pragma once
+
+
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <memory>
+#include <tuple>
+
+#include "triangle/triangle_internal.h"
+
+#include <OpenMesh/Core/IO/MeshIO.hh>
+#include <OpenMesh/Core/Mesh/TriMesh_ArrayKernelT.hh>
+
+namespace Migu {
+	using OpenMeshT = OpenMesh::DefaultTraits;
+	using TriMs = OpenMesh::TriMesh_ArrayKernelT<>;
+	using Tpt_d = std::tuple<int,int,double>;
+}
+using namespace Migu;
+
+template<typename ... Args>
+static std::string string_format(const std::string& format, Args ... args)
+{
+	auto size = snprintf(nullptr, 0, format.c_str(), args ...) + 1; // Extra space for '\0'
+	std::unique_ptr<char[]> buf(new char[size]);
+	snprintf(buf.get(), size, format.c_str(), args ...);
+	return std::string(buf.get(), buf.get() + size - 1); // We don't want the '\0' inside
+}
+
+static int bound(int i, int a, int b)
+{
+	return std::min(std::max(i, std::min(a, b)), std::max(a, b));
+}
+
+static cv::Scalar get_inverseColor(cv::Scalar c)
+{
+	return cv::Scalar(255, 255, 255, 0) - c;
+}
+
+struct imgLine
+{
+	size_t index;
+	double length;
+	size_t div;
+
+	imgLine(size_t index, double length, size_t div
+	) :index(index), length(length), div(div)
+	{}
+
+	bool operator<(imgLine const& a) const noexcept
+	{
+		return length / div > a.length / a.div;
+	}
+};
+
+struct imgText
+{
+	std::string text;
+	cv::Point orgi;
+	cv::Scalar color;
+
+	imgText(std::string text, cv::Point orgi, cv::Scalar color
+	) :text(text), orgi(orgi), color(color)
+	{}
+};
+
+static void draw_point(cv::Mat& img, cv::Point fp, cv::Scalar color, int rad = 2)
+{
+	circle(img, fp, rad, color, CV_FILLED, CV_AA, 0);
+}
+
+static void draw_line(cv::Mat& img, cv::Point p1, cv::Point p2, cv::Scalar color = cv::Scalar(0, 0, 0))
+{
+	line(img, p1, p2,
+		color, 2, cv::LineTypes::LINE_AA);
+}
+
+static void draw_text(cv::Mat& img, cv::String text, cv::Point org, cv::Scalar color = cv::Scalar(0, 0, 0))
+{
+	int baseline;
+	auto size = getTextSize(text, cv::HersheyFonts::FONT_HERSHEY_COMPLEX, 0.5, 1, &baseline);
+	cv::Point tmp_pt = { bound(org.x, 0, img.cols - size.width),
+					bound(org.y, size.height + baseline, img.rows - 1 - baseline) };
+	putText(img, text, tmp_pt, cv::HersheyFonts::FONT_HERSHEY_COMPLEX, 0.5, color, 1, cv::LineTypes::LINE_AA);
+}
+
+static void draw_text(cv::Mat& img, imgText label)
+{
+	draw_text(img, label.text, label.orgi, label.color);
+}
+static const std::string mainWindowName = "Affine with CUDA support";
+
+static bool if_in_range(cv::Point2d const& point, std::vector<cv::Point2d> const& points)
+{
+	return !points.empty() && norm(point - points[0]) < 30;
+}
